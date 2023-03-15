a2f4cbe820478a432ace268f12be907b760d2528
lucelin0414 <lucelin0414@gmail.com>
2023-03-15 14:29:08 +0900

    홍보페이지 수정

--- src/pages/Promote.js
+++ src/pages/Promote.js
@@ -4,7 +4,7 @@ const Promote =() =>{
     
     return(
         
-        <iframe width="100%" height="850" src="https://www.youtube.com/embed/O29XKYaJJ0Y? mute=1&loop=1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="100%" height="850" src="https://www.youtube.com/embed/O29XKYaJJ0Y? mute=1&autoplay=1&loop=1&controls=0&rel=0&playlist=O29XKYaJJ0Y&playsinline=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 
     );
