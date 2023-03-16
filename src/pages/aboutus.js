8949d0bb2fcc252e301ef73d579b683a2827b2a6
KiimMiinJii <mkim93810@gmail.com>
2023-03-16 12:21:39 +0900

    about

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -18,13 +18,21 @@ class aboutus extends Component {
                         <img src="img/logo_word.png" alt="logo" className='icon_word_main' />
                     </div>
                 </div>
+                {/* 시작문구 끝 */}
                 
-                {/* 페이지 헤더 */}
+                {/* 컨텐츠1 */}
                 <div class="page_header">
                     <div class="about_header">
+                        <p class="ann_head_about">
+                            한 잔의 커피를 만드는 원두는<br />
+                            나에게 60여가지의 좋은 아이디어를 가르쳐준다. <br /><br />
+                        </p>
+                        <p class="ann_head_about2">
+                            -루드비히 판 베토벤-
+                        </p>
                     </div>
                 </div>
-                {/* 페이지 헤더 끝 */}
+                {/* 컨텐츠1 끝 */}
 
                 {/* 본문 시작 */}
                 <div className='main_about'>
